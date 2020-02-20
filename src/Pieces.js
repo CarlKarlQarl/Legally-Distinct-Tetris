b57f1af2f0321211cc19a3f02daa619b37b64045
CarlKarlQarl <karlmatthes@gmail.com>
2020-02-20 13:26:09 -0700

    Random pieces can be displayed and moved around

---
+++ src/Pieces.js
@@ -0,0 +1,109 @@
+const allPieces = {
+    O: [
+        {
+            coordX: 4,
+            coordY: 0
+        },{
+            coordX: 5,
+            coordY: 0
+        },{
+            coordX: 4,
+            coordY: 1
+        },{
+            coordX: 5,
+            coordY: 1
+        }
+    ],
+    I: [
+        {
+            coordX: 5,
+            coordY: 0
+        },{
+            coordX: 5,
+            coordY: 1
+        },{
+            coordX: 5,
+            coordY: 2
+        },{
+            coordX: 5,
+            coordY: 3
+        }
+    ],
+    J: [
+        {
+            coordX: 4,
+            coordY: 2
+        },{
+            coordX: 5,
+            coordY: 2
+        },{
+            coordX: 5,
+            coordY: 1
+        },{
+            coordX: 5,
+            coordY: 0
+        }
+    ],
+    L: [
+        {
+            coordX: 4,
+            coordY: 0
+        },{
+            coordX: 4,
+            coordY: 1
+        },{
+            coordX: 4,
+            coordY: 2
+        },{
+            coordX: 5,
+            coordY: 2
+        }
+    ],
+    T: [
+        {
+            coordX: 3,
+            coordY: 0
+        },{
+            coordX: 4,
+            coordY: 0
+        },{
+            coordX: 5,
+            coordY: 0
+        },{
+            coordX: 4,
+            coordY: 1
+        }
+    ],
+    S: [
+        {
+            coordX: 3,
+            coordY: 1
+        },{
+            coordX: 4,
+            coordY: 1
+        },{
+            coordX: 4,
+            coordY: 0
+        },{
+            coordX: 5,
+            coordY: 0
+        }
+    ],
+    Z: [
+        {
+            coordX: 3,
+            coordY: 0
+        },{
+            coordX: 4,
+            coordY: 1
+        },{
+            coordX: 4,
+            coordY: 0
+        },{
+            coordX: 5,
+            coordY: 1
+        }
+    ]
+}
+
+export default allPieces
